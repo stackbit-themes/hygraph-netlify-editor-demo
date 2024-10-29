209c79aedea9e4b5d2594f0f8e05c56afe310544
Arseny Gurevich <arsenyg@gmail.com>
2024-10-29 15:53:04 +0100

    Card component - protective programming

--- components/Card.tsx
+++ components/Card.tsx
@@ -15,16 +15,18 @@ export default function Card({ image, title, url, cta, fieldPath }: Props) {
   return (
     <div className="bg-tertiary aspect-[1/1] relative" data-sb-field-path={fieldPath}>
       <a className="block absolute w-full h-full top-0 left-0" href={url}>
-        <Image
-          src={image.url}
-          alt={title}
-          width={500}
-          height={500}
-          className="absolute w-full h-auto"
-          sizes="100vw"
-          loading="lazy"
-          data-sb-field-path=".image"
-        />
+        {image && (
+          <Image
+            src={image.url}
+            alt={title}
+            width={500}
+            height={500}
+            className="absolute w-full h-auto"
+            sizes="100vw"
+            loading="lazy"
+            data-sb-field-path=".image"
+          />
+        )}
 
         <button className="cta absolute bottom-4 right-4" data-sb-field-path=".cta">{cta}</button>
         <div className="absolute top-6 md:top-auto md:-bottom-6 left-8">
